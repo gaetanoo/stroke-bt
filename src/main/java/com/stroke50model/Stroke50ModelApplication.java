e7fd1205ebe5a9515729a8a0e3c2b7f085d82766
gaetanoo <gaetano.cassalia@gmail.com>
2023-08-02 13:02:14 +0200

    first module created

---
+++ src/main/java/com/stroke50model/Stroke50ModelApplication.java
@@ -0,0 +1,13 @@
+package com.stroke50model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stroke50ModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stroke50ModelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stroke50model/Stroke50ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stroke50model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stroke50ModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
